677de5dba2a942e9006a189587b2753bdaaceaee
kani1 <kazakovakanykey@gmail.com>
2021-03-15 21:52:20 +0600

    добавила геттеры сеттеры и конструктор в класс Garage

--- src/com/company/Garage.java
+++ src/com/company/Garage.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Garage <A extends Car> implements Printable <A> {
     private A garage;
+    public Garage(){}
 
+    public Garage(A garage) {
+        this.garage = garage;
+    }
+
+    public A getGarage() {
+        return garage;
+    }
+
+    public void setGarage(A garage) {
+        this.garage = garage;
+    }
 
     @Override
     public void print(A a) {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Garage <Toyota> garageForToyota= new Garage<>();
         Toyota toyota= new Toyota();
+        garageForToyota.setGarage(toyota);
         garageForToyota.print(toyota);
 
 
         Garage<Porshe> garageForPoshe= new Garage<>();
         Porshe porshe= new Porshe();
+        garageForPoshe.setGarage(porshe);
         garageForPoshe.print(porshe);
 
 
         Garage <Mercedes> garageForMercedes= new Garage<>();
         Mercedes mercedes= new Mercedes();
+        garageForMercedes.setGarage(mercedes);
         garageForMercedes.print(mercedes);
         
 
